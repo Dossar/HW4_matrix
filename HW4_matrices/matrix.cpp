e5985e9ee70696d14f81b85e0a0cd79ff58b12cf
Roy Van Liew <vanliewroy@gmail.com>
2014-02-25 16:41:33 -0500

    Starting up the matrix class. The constructor functions work and the input from file also works.

--- HW4_matrices/matrix.cpp
+++ HW4_matrices/matrix.cpp
@@ -12,208 +12,133 @@
 
 using namespace std;
 
-/*
- * 
- */
-int main() {
+class Matrix
+{
+public:
+    Matrix(); // Default constructor
+    Matrix(int diagvalue); // Constructor with parameters
+    Matrix(const Matrix& M); // Copy constructor
+    ~Matrix(); // Destructor
+    void inputMat();
+    void printMat();
+    
+private:
+    int data[3][3];
+    
+};
 
-    int m1rows = 3, m1cols = 3, m2rows = 3, m2cols = 3;
-    int i, j, k;
-    int sum = 0;
-    int diff = 0;
-    int diagvalue;
-    int determinant;
-    
-    
-    // Initialize our test matrices.
-    int zeroMatrix[m1cols][m1cols];
-    int dMat[m1cols][m1cols];
-    int matrix1[m1rows][m1cols], matrix2[m2rows][m2cols];
-    int multMat[m1rows][m2cols]; // Result from multiplication
-    int addMat[m1rows][m1cols]; // Result from addition
-    int subMat[m1rows][m1cols]; // Result from addition
-    
-    
-    // CREATE ZERO MATRIX
+// Default constructor. Creates zero matrix.
+Matrix::Matrix()
+{
+    int i, j;
     for (i = 0; i < 3 ; i++) {
         for (j = 0; j < 3 ; j++)
-            zeroMatrix[i][j] = 0;
-    }     
-    
-    cout << "Zero Matrix:" << endl;
-    for (i = 0; i < 3 ; i++) {
-        for (j = 0; j < 3 ; j++) {
-            cout << zeroMatrix[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
+            data[i][j] = 0;
     }
-    cout << "\n";
-    // END CREATE ZERO MATRIX
-    
-    
-    // CREATE DIAGONAL MATRIX
-    cout << "Enter value you want in your diagonal:" << endl;
-    cin >> diagvalue;
-    cout << "\n";
-    
-    for (i = 0; i < 3 ; i++) {
+   
+}
+
+// Constructor with parameters. Creates diagonal matrix.
+Matrix::Matrix(int diagvalue)
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++) {
         // Initialize entire row to zero first, then insert number into diagonal.
-        for (j = 0; j < 3 ; j++)
-            dMat[i][j] = 0;
+        for (j = 0; j < 3; j++)
+            data[i][j] = 0;
         j = 0; // Reinitialize j to zero so we can get the correct column in the diagonal
         j += i;
-        dMat[i][j] = diagvalue;
-    }     
-    
-    cout << "Diagonal Matrix:" << endl;
+        data[i][j] = diagvalue;
+    }
+
+}
+
+// Copy constructor
+Matrix::Matrix(const Matrix& M)
+{
+    int i, j;
     for (i = 0; i < 3 ; i++) {
-        for (j = 0; j < 3 ; j++) {
-            cout << dMat[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
+        for (j = 0; j < 3 ; j++)
+            data[i][j] = M.data[i][j]; // Copy elements from our referenced matrix into our new matrix.
     }
-    cout << "\n";
-    // END CREATE DIAGONAL MATRIX
     
-    
-    /*
-      FIND DETERMINANT OF A 3X3 MATRIX
-      det(A) =  A11 * ( A22*A33 - A23*A32 )
-              - A12 * ( A21*A33 - A23*A31 )
-              + A13 * ( A21*A32 - A22*A31 )    
-    */
-    determinant = ( (dMat[0][0] * ( dMat[1][1] * dMat[2][2] - dMat[1][2] * dMat[2][1] ) )
-                   -(dMat[0][1] * ( dMat[1][0] * dMat[2][2] - dMat[1][2] * dMat[2][0] ) )
-                   +(dMat[0][2] * ( dMat[1][0] * dMat[2][1] - dMat[1][1] * dMat[2][0] ) ) );
-    
-    cout << "The determinant of your diagonal matrix is: " << determinant << endl;
-    cout << "\n";
-    // END DETERMINANT
-    
-    
-    // INPUT MATRICES
-    cout << "Input numbers for matrix 1." << endl;
-    for (i = 0; i < m1rows; i++) {
-        cout << "Row " << i+1 << ", enter " << m1cols << " numbers:" << endl;
-        for (j = 0; j < m1cols; j++) {
-            cin >> matrix1[i][j];
-        }
-    }
-    cout << endl;
+}
 
-    cout << "Input numbers for matrix 2." << endl;
-    for (i = 0; i < m2rows; i++) {
-        cout << "Row " << i+1 << ", enter " << m2cols << " numbers:" << endl;
-        for (j = 0; j < m2cols; j++) {
-            cin >> matrix2[i][j];
-        }
-    }
-    cout << endl;
-    
-    // Print out our two matrices to see they were input correctly
-    cout << "Matrix 1:" << endl;
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m1cols; j++) {
-            cout << matrix1[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
-    }
-    cout << "\n";
-    
-    cout << "Matrix 2:" << endl;
-    for (i = 0; i < m2rows; i++) {
-        for (j = 0; j < m2cols; j++) {
-            cout << matrix2[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
-    }
-    cout << "\n";
-    // END OF INPUT MATRICES
+// Destructor
+Matrix::~Matrix()
+{
+        cout << "A matrix has been destroyed." << endl;
+}
 
-    
-    // MATRIX MULTIPLICATION
-    // For each row of matrix1 and for each column of matrix2, multiply the matrices.
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m2cols; j++) {
-            sum = 0;
-            // We traverse down the rows of said column in matrix 2 to get the sum for the product matrix.
-            for (k = 0; k < m2rows; k++)
-                sum += matrix1[i][k] * matrix2[k][j];
-            multMat[i][j] = sum;
-        }
-    }
 
-    // Print out the matrix.
-    cout << "Resulting product matrix is " << m1rows << "x" << m2cols <<"." << endl;
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m2cols; j++) {
-            cout << multMat[i][j] << " ";
+
+// Make fin a global variable so we can save our position in our input file.
+ifstream fin("file.txt");
+
+// Read in stuff from our input file into our matrices.
+void Matrix::inputMat(){
+    
+    int i, j;
+    
+    // Read in 9 elements from our file.
+    for( i = 0 ; i < 3 ; i++ ){
+        for( j = 0 ; j < 3 ; j++ ){
+            // fin ignores whitespace and newlines
+            // We also keep our position in the file with fin
+            fin >> data[i][j];
+        }
+    }
+    
+}
+
+// Print out whatever matrix we wanted.
+void Matrix::printMat(){
+    
+    int i, j;
+    //cout << "Matrix:" << endl;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            cout << data[i][j] << " ";
         }
         cout << endl; // Newline for next row
     }
     cout << "\n";
-    // END OF MATRIX MULTIPLICATION
     
+}
+
+
+int main() {
+
+    // Z is zero matrix.
+    Matrix Z;
+    cout << "Matrix Z:" << endl;
+    Z.printMat();
     
-    // MATRIX ADDITION
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m1cols; j++){
-            sum = matrix1[i][j] + matrix2[i][j];
-            addMat[i][j] = sum;
-        }
-    }
+    // D is 2 diagonal, E is 1 diagonal.
+    Matrix D(2);
+    cout << "Matrix D:" << endl;
+    D.printMat();
+    Matrix E(1);
+    cout << "Matrix E:" << endl;
+    E.printMat();
     
-    cout << "Matrix 1 + Matrix 2 gave:" << endl;
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m1cols; j++) {
-            cout << addMat[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
-    }
-    cout << "\n";
-    // END OF MATRIX ADDITION
+    // A and C are the input file matrices. Initialize to zero first, then input stuff.
+    Matrix A;
+    Matrix C;
+    A.inputMat();
+    cout << "Matrix A:" << endl;
+    A.printMat();
+    C.inputMat();
+    cout << "Matrix C:" << endl;
+    C.printMat();
+    fin.close(); // We are now done with our input file, so close it.
     
+    // B is a copy of A.
+    Matrix B(A);
+    cout << "Matrix B:" << endl;
+    B.printMat();
     
-    // MATRIX SUBTRACTION
-    for (i = 0; i < m1rows; i++) { 
-        for (j = 0; j < m1cols; j++){
-            diff = matrix1[i][j] - matrix2[i][j];
-            subMat[i][j] = diff;
-        }
-    }
-    
-    cout << "Matrix 1 - Matrix 2 gave:" << endl;
-    for (i = 0; i < m1rows; i++) {
-        for (j = 0; j < m1cols; j++) {
-            cout << subMat[i][j] << " ";
-        }
-        cout << endl; // Newline for next row
-    }
-    // END OF MATRIX SUBTRACTION
-    
-    
-    /*
-     The follow block of code was originally for scanning in dimensions. We don't need this right now.
-     */
-    
-//    // Enter in the dimensions of our two matrices.
-//    cout << "Enter dimensions of your two matrices to multiply." << endl;
-//    cout << "Remember the columns of 1st matrix need to match rows of 2nd matrix." << endl;
-//    cout << "Matrix 1 rows: ";
-//    cin >> m1rows; // Rows of matrix1
-//    cout << "Matrix 1 columns: ";
-//    cin >> m1cols; // Columns of matrix1
-//    cout << "Matrix 2 rows: ";
-//    cin >> m2rows; // Rows of matrix2
-//    cout << "Matrix 2 columns: ";
-//    cin >> m2cols; // Columns of matrix
-//    int matrix1[m1rows][m1cols], matrix2[m2rows][m2cols];
-//    int multMat[m1rows][m2cols]; // Result from multiplication
-//    int addMat[m1rows][m1cols]; // Result from addition
-//    int subMat[m1rows][m1cols]; // Result from addition
-//    cout << endl;
-//
-//     
     
     return 0;
 }
